218f1398b9e688961f34e0a625a3986ece0712f4
Axel Filipsson <filipsson960409@gmail.com>
2022-05-10 10:10:32 +0200

    final fix

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/acitivies', require('./routes/activityRoutes'))
+app.use('/api/activities', require('./routes/activityRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
 // Serve frontend
